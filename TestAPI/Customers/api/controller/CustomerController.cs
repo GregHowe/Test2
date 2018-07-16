2e2b07501ca2c4ebd4c19747876cc1e29e0d209b
Richard Fernandez <richardfc83@gmail.com>
2018-07-16 01:53:33 -0500

    First Commit

---
+++ TestAPI/Customers/api/controller/CustomerController.cs
@@ -0,0 +1,126 @@
+namespace Customer.Api
+{
+    using Customer.Application;
+    using Customer.Application.Dto;
+    using Common.Application;
+    using Common.Application.Dto;
+    using Common.Api.Controller;
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using Common.Infrastructure.Repository;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Http;
+    using Common.constantes;
+
+    [Route("api/Customers/customer")]
+    public class CustomerController : Controller
+    {
+
+        ICustomerApplicationService _customerApplicationService;
+        ResponseHandler responseHandler;
+
+        public CustomerController(ICustomerApplicationService customerApplicationService)
+        {
+            _customerApplicationService = customerApplicationService;
+            responseHandler = new ResponseHandler();
+        }
+
+        [HttpGet("{CustomerId}")]
+        public IActionResult Get(long CustomerId)
+        {
+            try
+            {
+                return Ok(_customerApplicationService.getById(CustomerId));
+            }
+            catch (Exception)
+            {
+                return StatusCode(Constantes.HttpStatus.ErrorServer, this.responseHandler.getAppExceptionResponse());
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Get(int offset, int limit)
+        {
+            try
+            {
+                return Ok(_customerApplicationService.getAll(offset, limit));
+            }
+            catch (Exception)
+            {
+                return StatusCode(Constantes.HttpStatus.ErrorServer, this.responseHandler.getAppExceptionResponse());
+            }
+        }
+
+        [Route("/api/Customers/findByDocumentNumber")]
+        [HttpGet]
+        public IActionResult  findByDocumentNumber(string documentNumber)
+        {
+            try
+            {
+                return Ok(_customerApplicationService.findByDocumentNumber(documentNumber));
+            }
+            catch (Exception)
+            {
+                return StatusCode(Constantes.HttpStatus.ErrorServer, this.responseHandler.getAppExceptionResponse());
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CustomerDto customerDto)
+        {
+            try
+            {
+                _customerApplicationService.create(customerDto);
+                return Ok(this.responseHandler.getOkCommandResponse("Customer created!", Constantes.HttpStatus.Created));
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(this.responseHandler.getAppCustomErrorResponse(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(Constantes.HttpStatus.ErrorServer, this.responseHandler.getAppExceptionResponse());
+            }
+        }
+
+        [HttpPut("{CustomerId}")]
+        public IActionResult Put([FromBody] CustomerDto customerDto, long CustomerId)
+        {
+            try
+            {
+                _customerApplicationService.update(customerDto, CustomerId);
+                return Ok(this.responseHandler.getOkCommandResponse("Customer updated!", Constantes.HttpStatus.Success));
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(this.responseHandler.getAppCustomErrorResponse(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(Constantes.HttpStatus.ErrorServer, this.responseHandler.getAppExceptionResponse());
+            }
+        }
+
+        [HttpDelete("{CustomerId}")]
+        public IActionResult Delete(int CustomerId)
+        {
+            try
+            {
+                _customerApplicationService.deleted(CustomerId);
+                return Ok(this.responseHandler.getOkCommandResponse("Customer deleted!", Constantes.HttpStatus.Success));
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(this.responseHandler.getAppCustomErrorResponse(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(Constantes.HttpStatus.ErrorServer, this.responseHandler.getAppExceptionResponse());
+            }
+        }
+    }
+}
+
---
+++ TestAPI/Startup.cs
@@ -0,0 +1,82 @@
+﻿
+namespace TestAPI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using Microsoft.EntityFrameworkCore;
+    using Customer.Domain.Repository;    
+    using AutoMapper;
+    using Automapper;
+    using Common;
+    using Common.Infrastructure.Repository;
+    using Common.infrastructure.repository;
+    using Customer.Application;
+    using Transactions.application;
+    using Transactions.Application;
+    //using Transactions.Infraestructure;
+    using BankAccount.Domain.Repository;
+    using BankAccount.Application;
+    using Security.application;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+     
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BankingContext>(options => options.UseMySql(Configuration.GetConnectionString("MySqlConnection")));                        
+            services.AddScoped<ICustomerApplicationService, CustomerApplicationService>();
+            services.AddScoped<IBankAccountApplicationService,BankAccountApplicationService>();
+            services.AddScoped<ITransactionApplicationService, TransactionApplicationService>();
+            services.AddScoped<ISecurityApplicationService,  SecurityApplicationService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IBankAccountRepository, BankAccountRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddCors();
+
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutomapperProfile());
+            });
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<DbInitializer>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env ,ILoggerFactory loggerFactory,  DbInitializer seeder)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
